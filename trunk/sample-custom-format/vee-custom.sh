b287ec7f8b583dd87d266f8202e58ea60d49c14c
B. Estrade <estrabd@gmail.com>
2007-09-12 20:18:39 +0000

    testing and playing...right now it is looking good

--- trunk/sample-custom-format/vee-custom.sh
+++ trunk/sample-custom-format/vee-custom.sh
@@ -1,14 +1,13 @@
 #FORMAT_CUSTOM_DISPATCHER=custom_set_format_func
 #FORMAT_CUSTOM_DEFS=./vee-custom.sh 
-function format_with_custom
-{
- echo hello
- die_cleanly
+
+function format_with_null # example implements the same as -f none
+{ cat ${RAW} >> ${FINAL}
 }
 
 function custom_set_format_func
 { case "$1" in
-    custom) FORMAT_FUNC=format_with_custom
+    null) FORMAT_FUNC=format_with_null
             echo using $FORMAT_FUNC
             ;;
     *) echo "bad format type" 
