dc1cc8504871e82ca9233173a38495b14abbbb54
B. Estrade <estrabd@gmail.com>
2007-09-12 20:29:44 +0000

    added filter manager, allowing for easy management of the successive application of multiple filters

---
+++ trunk/sample-filters/vee-filtermanager.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+# helps facilitate the applications of multiple filters in-line
+# set as teh main vee-filter, and it will apply the stream to the
+# set filters in the order listed
+
+use strict;
+use warnings;
+$|++;
+
+# define filter dir here
+my $FILTERDIR = "sample-filters/";
+
+# add new filters to the list - note, they are run in top-down order
+# warning - filters are not checked for existence first
+my @FILTERS = (
+               "$FILTERDIR/vee-filter.sh",
+               "$FILTERDIR/vee-filter.pl",
+               ,);
+
+#
+# automated from here on out - do not touch
+#
+my $FILTERS = join("|",@FILTERS);
+my $out = "";
+# loop over STDIN and capture qscript
+while (<>) {
+  $out .= $_;
+}
+# open pipe to filters, execute
+open(FILTER, "|$FILTERS");
+  # send contents of qscript to filters
+  print FILTER  $out;
+close(FILTER);
+
+1;
