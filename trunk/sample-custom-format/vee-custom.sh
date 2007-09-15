c2d00659aef426def5236b869c8122126ea406e5
B. Estrade <estrabd@gmail.com>
2007-09-15 04:58:53 +0000

    need to test some more, but things are shaping up nicely

--- trunk/sample-custom-format/vee-custom.sh
+++ trunk/sample-custom-format/vee-custom.sh
@@ -1,5 +1,8 @@
 #FORMAT_CUSTOM_DISPATCHER=custom_set_format_func
 #FORMAT_CUSTOM_DEFS=./vee-custom.sh 
+#UPDATE_INDEX=custom_update_index
+#OUTPUT_TOP=custom_output_top
+#OUTPUT_BOT=custom_output_bottom
 
 function format_with_null # example implements the same as -f none
 { cat ${RAW} >> ${FINAL}
@@ -31,3 +34,42 @@ function custom_set_format_func
        ;;
   esac 
 }
+
+# formats post link on index page
+function custom_update_index
+{ echo "<!-- ;${SEC}; -->${PUBLISHED}:<a href=\"${DIR}/${SEC}.${TIME}.${FORMAT}\"> ${TITLE}</a>" >> ${INDEX}
+  if [ -n "${SUMMARY}" ]; then
+    SEC=`expr $SEC - 1`
+    echo "<!-- ;${SEC}; -->${SUMMARY}" >> ${INDEX}
+  fi
+}
+
+function custom_output_top
+{ echo ${HEADERTXT} > ${FINAL}
+  if [ -e "${TOP_TPL}" ]; then
+     cat "${TOP_TPL}" >> ${FINAL}
+     echo "<pre>"   >> ${FINAL}
+   else
+     echo "<!-- <?xml version=\"1.0\" encoding=\"UTF-8\" ?> --><!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\">"  >> ${FINAL}
+     echo "<meta http-equiv=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\">" >> ${FINAL}
+     echo "<head><title>${TITLE} - ${DATE}</title></head>"  >> ${FINAL}
+     echo "<body>"  >> ${FINAL}
+     echo "<pre>"   >> ${FINAL}
+     echo "<a href=\"../${INDEX}\">[index]</a><a href=\"./${RAWNAME}\">[raw]</a><a href=\"../\">[main]</a>" >> ${FINAL}
+   fi
+   echo           >> ${FINAL}
+}
+
+function custom_output_bottom
+{ echo          >> ${FINAL}
+  echo --       >> ${FINAL}
+  echo ${FOOTERTXT} >> ${FINAL}
+  if [ -e "${BOT_TPL}" ]; then
+    cat "${BOT_TPL}" >> ${FINAL}
+  else
+    echo "</pre>" >>  ${FINAL}
+    echo "</body>" >>  ${FINAL}
+    echo "</html>" >>  ${FINAL}
+  fi
+}
+
