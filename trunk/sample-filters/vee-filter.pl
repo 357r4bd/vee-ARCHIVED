ab950596a31fb5f819d8ccebfa1e93ae7707e96b
B. Estrade <estrabd@gmail.com>
2007-09-22 03:11:45 +0000

    reorg

--- trunk/sample-filters/vee-filter.pl
+++ trunk/sample-filters/vee-filter.pl
@@ -1,12 +1,14 @@
 #!/bin/env perl
 
-# test filter that replaces AUBURN with an img tag
+# toy filter that replaces AUBURN with an img tag
+
+my $FORMAT_FUNC = $ARGV[0]; # name of format function is passed in
 
 while(<STDIN>) {
   chomp;
   if ($_ =~ s/AUBURN/<img src=http:\/\/www.emaspro.com\/Portals\/0\/images\/Logo\%20-\%20Auburn.jpg>/g ) {
   # argv[0] is the name of the formatting function
-    if ($ARGV[0] =~ m/groff/) {
+    if ($FORMAT_FUNC =~ m/groff/) {
       printf("'nf\n%s\n'fi\n",$_); 
     } else {
       printf("%s\n",$_); 
--- trunk/sample-filters/vee-filter.sh
+++ trunk/sample-filters/vee-filter.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# this filter is a null filter; meaning it spits out
+# exactly what was passed in
+
+FORMAT_FUNC=$1 # the name of the format function used is passed in
+
 IFS=""                     # ensures that leading spaces are retained
 while read -r IN <&0 ; do  # break after 1 sec of no stdin
  echo "${IN}"              # echo's stdin back out so user can see 
