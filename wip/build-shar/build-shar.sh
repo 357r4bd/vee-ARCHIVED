51bfa06026bb96e43c38474a1ecd7d46e4b9fd08
B. Estrade <estrabd@gmail.com>
2014-07-08 15:27:22 -0500

    creating the infrastructure required to easily generate the shar file that will available to install vee locally

---
+++ wip/build-shar/build-shar.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# this script will build the downloadable shar that will install vee locally
+
+rm -rf ./bin ./man ./pod ./tmp .veeprofile > /dev/null 2>&1
+
+mkdir ./bin ./man 
+
+# move utils
+cp ../../bin/vee ./bin/vee
+cp ../../bin/veecat ./bin/veecat
+cp ../../bin/veefind ./bin/veefind
+# set perms
+chmod 700 ./bin/*
+
+# generate documentation
+cp -r ../../pod .
+cd pod
+make all
+
+mv -f *.[123456789] ../man
+cd ..
+
+echo "PATH=\$PATH:\$HOME/bin" > .veeprofile
+echo "MANPATH=\$MANPATH:\$HOME/man" >> .veeprofile
+
+shar ./bin ./man ./bin/* ./man/* .veeprofile > ../install-vee.shar
+
+rm -rf ./bin ./man ./pod .veeprofile > /dev/null 2>&1
