0771cd0d7bc338b1af62e0ab24b7898c4fb32199
B. Estrade <estrabd@gmail.com>
2014-07-11 22:31:11 -0500

    This commit demonstrates the creation of a shar file that may be downloaded by curl and piped directly into /bin/sh. The "HI, THERE" is the execution of the actual install script.
    For example,
    
     https://url/to/file/dont-use-install-vee.shar | sh
    c - ./vee-builddir
    c - ./vee-builddir/bin
    x - ./vee-builddir/bin/vee
    x - ./vee-builddir/bin/veecat
    x - ./vee-builddir/bin/veefind
    c - ./vee-builddir/man
    x - ./vee-builddir/man/vee-reindex.1
    x - ./vee-builddir/man/vee.1
    x - ./vee-builddir/man/veecat.1
    x - ./vee-builddir/man/veels.1
    x - ./vee-builddir/man/veerc.5
    c - ./vee-builddir/pod
    x - ./vee-builddir/pod/veefind.1.pod
    x - ./vee-builddir/pod/README
    x - ./vee-builddir/pod/vee-hooks.5.pod
    x - ./vee-builddir/pod/vee-reindex.1.pod
    x - ./vee-builddir/pod/vee.1.pod
    x - ./vee-builddir/pod/veecat.1.pod
    x - ./vee-builddir/pod/veels.1.pod
    x - ./vee-builddir/pod/veerc.5.pod
    x - ./vee-builddir/pod/Makefile
    x - ./vee-builddir/install-vee.sh
    x - ./vee-builddir/.veeprofile
    HI, THERE

--- build-shar/build-shar.sh
+++ build-shar/build-shar.sh
@@ -2,28 +2,44 @@
 
 # this script will build the downloadable shar that will install vee locally
 
-rm -rf ./bin ./man ./pod ./tmp .veeprofile > /dev/null 2>&1
+WORKDIR=./vee-builddir
 
+rm -rf $WORKDIR  > /dev/null 2>&1
+
+mkdir $WORKDIR
+cd $WORKDIR
 mkdir ./bin ./man 
 
 # move utils
-cp ../bin/vee ./bin/vee
-cp ../bin/veecat ./bin/veecat
-cp ../bin/veefind ./bin/veefind
+cp ../../bin/vee ./bin/vee
+cp ../../bin/veecat ./bin/veecat
+cp ../../bin/veefind ./bin/veefind
 # set perms
 chmod 700 ./bin/*
 
 # generate documentation
-cp -r ../pod .
+cp -r ../../pod .
 cd pod
-make all
+make all > /dev/null 2>&1
 
 mv -f *.[123456789] ../man
 cd ..
 
+cp ../install-vee.sh .
+
 echo "PATH=\$PATH:\$HOME/bin" > .veeprofile
 echo "MANPATH=\$MANPATH:\$HOME/man" >> .veeprofile
 
-shar ./bin ./man ./bin/* ./man/* .veeprofile > ./dont-use-install-vee.shar
+cd ..
 
-rm -rf ./bin ./man ./pod .veeprofile > /dev/null 2>&1
+# FreeBSD shar(1)
+shar `find $WORKDIR -print` > ./dont-use-install-vee.shar.tmp
+
+echo "# kick off install process" >> ./dont-use-install-vee.shar.tmp
+echo "cd $WORKDIR" >> ./dont-use-install-vee.shar.tmp
+echo "sh ./install-vee.sh" >> ./dont-use-install-vee.shar.tmp
+
+# filter out the exit at the end that shar(1) added
+grep -v "^exit" ./dont-use-install-vee.shar.tmp > ./dont-use-install-vee.shar
+
+rm -rf $WORKDIR > /dev/null 2>&1
