97c3d8ca4ce225ac05ff369720f8832ccf4c328f
B. Estrade <estrabd@gmail.com>
2014-07-14 17:43:45 -0500

    install incremental save

--- build-shar/install-vee.sh
+++ build-shar/install-vee.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
-echo HI, THERE
+IFS=
+
+echo Checking local environment...
+
+PREFIX=$(pwd)/bin
+MANPATH=$(pwd)/man
+
+if [ ! -d "$PREFIX" ]; then
+  echo "$PREFIX doesn't exist, create it? [yN] "
+  read yN
+   
+  if [ "$yN" == "y" ]; then
+    mkdir $PREFIX
+  fi 
+fi
+
+if [ ! -d "$MANPATH" ]; then
+  echo "$MANPATH doesn't exist, create it? [yN] "
+  read yN
+   
+  if [ "$yN" == "y" ]; then
+    mkdir $MANPATH
+  fi 
+fi
+
