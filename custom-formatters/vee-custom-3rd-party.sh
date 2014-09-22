a41f867a3f46386c1a2a5716f8fb1b49542bd833
Matthias Beyer <mail@beyermatthias.de>
2014-09-22 16:16:44 +0200

    Added support for `markdown` tool

--- custom-formatters/vee-custom-3rd-party.sh
+++ custom-formatters/vee-custom-3rd-party.sh
@@ -24,11 +24,25 @@
      fi
 }
 
+# uses markdown, a markdown-to-html converter tool
+ format_with_mardown() # e
+{
+    MARKDOWN=`which markdown`;
+    if [ "$MARKDOWN" == "" ]; then
+        echo markdown is not installed
+        die_cleanly
+    else
+        cat ${RAW} | ${MARKDOWN} >> ${FINAL}
+    fi
+}
+
  custom_set_format_func()
 { case "$1" in
     par72) FORMAT_FUNC=format_with_par72
             ;;
     pandoc) FORMAT_FUNC=format_with_pandoc
+            ;;
+    markdown) FORMAT_FUNC=format_with_markdown
             ;;
     *) echo "$1 bad format type" 
        die_cleanly
